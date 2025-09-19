66e9556135fb4fcad2dc18168ee0eb8754cbb1b6
Tortoise father <joshhunter6363@gmail.com>
2025-09-19 02:26:24 +0100

    i hate website

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import './styles/globals.css'
+
 import Home from './routes/Home'
 import StepPage from './routes/StepPage'
 
-
-const router = createBrowserRouter([
-{ path: '/', element: <Home /> },
-{ path: '/steps/:slug', element: <StepPage /> },
-])
-
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-<React.StrictMode>
-<RouterProvider router={router} />
-</React.StrictMode>,
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/steps/:slug" element={<StepPage />} />
+      </Routes>
+    </HashRouter>
+  </StrictMode>
 )
